5375dfddc3246c8340dcf5fa816004a97f594fdf
Юрий Фоменков <fomenkov2003f@mail.ru>
2021-02-16 12:59:07 +0300

    Add BLL ModelsJson Tinkoff Banks

---
+++ SellSpasibo.BLL/Models/ModelsJson/Tinkoff/AnyBanks/TinkoffBankJson.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SellSpasibo.BLL.Models.ModelsJson.Tinkoff.AnyBanks
+{
+    public class TinkoffBankJson
+    {
+        [JsonPropertyName("bankMemberId")]
+        public string BankMemberId { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
--- SellSpasibo.BLL/Models/ModelsJson/Tinkoff/AnyBanks/TinkoffGetBanks.cs
+++ SellSpasibo.BLL/Models/ModelsJson/Tinkoff/AnyBanks/TinkoffGetBanks.cs
@@ -1,6 +1,13 @@
-﻿namespace SellSpasibo.BLL.Models.ModelsJson.Tinkoff.AnyBanks
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace SellSpasibo.BLL.Models.ModelsJson.Tinkoff.AnyBanks
 {
     public class TinkoffGetBanks
     {
+        [JsonPropertyName("Payload")]
+        public List<TinkoffBankJson> Payload { get; set; }
+        [JsonPropertyName("resultCode")]
+        public string ResultCode { get; set; }
     }
 }
